7054278912379ed3673f0c38408f36cda9a7ed4d
Tirtho Mojumdar <tirthomojumder9876@gmail.com>
2025-12-11 14:10:49 +0600

    Product using Decorator pattern

---
+++ Product.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'Database.php';
+interface IProduct {
+    public function getPrice();
+    public function getDescription();
+}
+
+class BasicProduct implements IProduct {
+    protected $name;
+    protected $price;
+
+    public function __construct($name, $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getPrice() { return $this->price; }
+    public function getDescription() { return $this->name; }
+}
+
+class WarrantyDecorator implements IProduct {
+    protected $product;
+
+    public function __construct(IProduct $product) {
+        $this->product = $product;
+    }
+
+    public function getPrice() {
+        return $this->product->getPrice() + 2000; // Warranty costs 2000 Tk
+    }
+
+    public function getDescription() {
+        return $this->product->getDescription() . " + 1 Year Official Warranty";
+    }
+}
+?>
